ae61c8147766e06231c786ea3961ae17e1ce2490
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-01-02 10:13:30 +0100

    other

---
+++ other/latest_version.php
@@ -0,0 +1,6 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+{
+    "LASTVERSION": "meterN 1.2.2",
+    "LINK": "https://www.metern.org/downloads/metern1.2.2.tar.gz",
+    "md5": "a2052b4403bd0b62e4580b5ae41b2995"
+}
