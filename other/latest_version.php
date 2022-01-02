05276ffe8ed1c8e48739f1bee5feb33f73b26d3a
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-01-02 16:00:06 +0100

    Update latest_version.php

--- other/latest_version.php
+++ other/latest_version.php
@@ -1,6 +1,6 @@
 <?php header('Access-Control-Allow-Origin: *'); ?>
 {
-    "LASTVERSION": "meterN 1.2.2",
-    "LINK": "https://www.metern.org/downloads/metern1.2.2.tar.gz",
-    "md5": "a2052b4403bd0b62e4580b5ae41b2995"
+    "LASTVERSION": "meterN 1.2.3",
+    "LINK": "https://www.metern.org/downloads/metern1.2.3.tar.gz",
+    "md5": "d691a8f691f74435a71d099299de71d4"
 }
