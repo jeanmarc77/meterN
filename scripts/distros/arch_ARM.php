1be7281af1a0dab795e69c4ce4923de6a64f4ce1
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-03-14 18:22:38 +0100

    disk use

--- scripts/distros/arch_ARM.php
+++ scripts/distros/arch_ARM.php
@@ -12,6 +12,6 @@ $CPUUSE="ps aux|awk 'NR > 0 { s +=$3 }; END {print \"cpu %\",s}' | awk '{ print 
 $MEMTOT="free -m | grep 'Mem' | awk '{print $2}'";
 $MEMUSE="free -m| grep 'Mem' | awk '{print $3}'";
 $MEMFREE="free -m| grep 'Mem' | awk '{print $7}'";
-$DISKUSE="df -h | grep root | awk '{print $2}'";
-$DISKFREE="df -h | grep root | awk '{print $4}'";
+$DISKUSE="df -h | grep '/$' | awk '{print $2}'";
+$DISKFREE="df -h | grep '/$' | awk '{print $4}'";
 ?>
--- scripts/distros/arch_ARM_sysstat.php
+++ scripts/distros/arch_ARM_sysstat.php
@@ -12,6 +12,6 @@ $CPUUSE="sar 1 1 | tail -n1 | awk '{print $3+$5}'";
 $MEMTOT="free -m | grep 'Mem' | awk '{print $2}'";
 $MEMUSE="free -m| grep 'Mem' | awk '{print $3}'";
 $MEMFREE="free -m| grep 'Mem' | awk '{print $7}'";
-$DISKUSE="df -h | grep /dev/sda | awk '{print $2}'";
-$DISKFREE="df -h | grep /dev/sda | awk '{print $4}'";
+$DISKUSE="df -h | grep '/$' | awk '{print $2}'";
+$DISKFREE="df -h | grep '/$' | awk '{print $4}'";
 ?>
