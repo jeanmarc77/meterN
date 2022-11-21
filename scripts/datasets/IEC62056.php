0f5dd57d3d89a663067ab5b4bc5d3127fce91d0f
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2022-11-21 11:35:17 +0100

    Update IEC62056.php
    allow null value

--- scripts/datasets/IEC62056.php
+++ scripts/datasets/IEC62056.php
@@ -1,10 +1,13 @@
 <?php
 function isvalid($id, $datareturn) //  IEC 62056 data set structure
 {
-	$regexp = "/^$id\(.+\*.+\)$/i"; //ID(VALUE*UNIT)
+	$regexp = "/^$id\(.+".'\x00'."*.+\)$/i"; //ID(VALUE*UNIT)
 	if (preg_match($regexp, $datareturn)) {
 		$datareturn = preg_replace("/^$id\(/i", '', $datareturn, 1); // VALUE*UNIT)
 		$datareturn = preg_replace("/\*.+\)$/i", '', $datareturn, 1); // VALUE
+		if(empty($datareturn)) {
+			$datareturn = 'empty';
+		}
 	} else {
 		$datareturn = null;
 	}
