a8c404be5bd456ffb79f0a270f7ced3ac16f5baa
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2023-05-14 16:47:44 +0200

    Update IEC62056.php

--- scripts/datasets/IEC62056.php
+++ scripts/datasets/IEC62056.php
@@ -5,7 +5,7 @@ function isvalid($id, $datareturn) //  IEC 62056 data set structure
 	if (preg_match($regexp, $datareturn)) {
 		$datareturn = preg_replace("/^$id\(/i", '', $datareturn, 1); // VALUE*UNIT)
 		$datareturn = preg_replace("/\*.+\)$/i", '', $datareturn, 1); // VALUE
-		if(!isset($datareturn)) {
+		if(!is_numeric($datareturn)) {
 			$datareturn = null;
 		}
 	} else {
