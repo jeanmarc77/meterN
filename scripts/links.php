311117e9eed7885c66426dc473f9ed2b76ed93b3
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2026-04-01 10:52:01 +0200

    Update links.php
    fixed #28

--- scripts/links.php
+++ scripts/links.php
@@ -5,14 +5,21 @@
  * @package default
  */
 
-
 $JSjquery = "https://code.jquery.com/jquery-3.7.1.min.js' integrity='sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=' crossorigin='anonymous";
 $JSjqui   = "https://code.jquery.com/ui/1.14.1/jquery-ui.min.js'
   integrity='sha256-AlTido85uXPlSyyaZNsjJXeCs07eSv3r43kyCVc8ChI='
   crossorigin='anonymous";
 $JSjquit  = "https://code.jquery.com/ui/1.14.1/themes/south-street/jquery-ui.css";
 $HC       = "https://code.highcharts.com/highcharts.js";
+if (!is_readable($HC)) {
+	$HC = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/highcharts.js';
+	$HCmore = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/highcharts-more.js';
+	$HCdd = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/modules/drilldown.js';
+	$HCexp = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/modules/exporting.js';
+} else {
 $HCmore   = "https://code.highcharts.com/highcharts-more.js";
 $HCdd     = "https://code.highcharts.com/modules/drilldown.js";
 $HCexp    = "https://code.highcharts.com/modules/exporting.js";
+}
+
 ?>
