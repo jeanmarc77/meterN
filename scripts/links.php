6772941d993648b1a37dee136438b6c51d9ba63b
Jean-Marc Louviaux <jeanmarc.louviaux@gmail.com>
2026-04-05 11:23:08 +0200

    Update links.php

--- scripts/links.php
+++ scripts/links.php
@@ -5,21 +5,24 @@
  * @package default
  */
 
-$JSjquery = "https://code.jquery.com/jquery-3.7.1.min.js' integrity='sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=' crossorigin='anonymous";
-$JSjqui   = "https://code.jquery.com/ui/1.14.1/jquery-ui.min.js'
-  integrity='sha256-AlTido85uXPlSyyaZNsjJXeCs07eSv3r43kyCVc8ChI='
-  crossorigin='anonymous";
-$JSjquit  = "https://code.jquery.com/ui/1.14.1/themes/south-street/jquery-ui.css";
-$HC       = "https://code.highcharts.com/highcharts.js";
-if (!is_readable($HC)) {
-	$HC = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/highcharts.js';
-	$HCmore = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/highcharts-more.js';
-	$HCdd = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/modules/drilldown.js';
-	$HCexp = 'https://cdn.jsdelivr.net/npm/highcharts@12.5.0/modules/exporting.js';
+if (false) { // Set to true if you wish to use local lib
+	$HC     = 'js/highcharts/highcharts.js';
+	$HCmore = 'js/highcharts/highcharts-more.js';
+	$HCdd   = 'js/highcharts/modules/drilldown.js';
+	$HCexp  = 'js/highcharts/modules/exporting.js';
+	$HCann  = 'js/highcharts/annotations.js';
+	$JSjquery = 'js/jquery/jquery-3.6.0.min.js';
+	$JSjqui   = 'js/jquery/jquery-ui.min.js';
+	$JSjquit  = 'js/jquery/jquery-ui.css';
 } else {
-$HCmore   = "https://code.highcharts.com/highcharts-more.js";
-$HCdd     = "https://code.highcharts.com/modules/drilldown.js";
-$HCexp    = "https://code.highcharts.com/modules/exporting.js";
+	$JSjquery = "https://code.jquery.com/jquery-3.7.1.min.js' integrity='sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=' crossorigin='anonymous";
+	$JSjqui   = "https://code.jquery.com/ui/1.14.1/jquery-ui.min.js' integrity='sha256-AlTido85uXPlSyyaZNsjJXeCs07eSv3r43kyCVc8ChI=' crossorigin='anonymous";
+	$JSjquit  = "https://code.jquery.com/ui/1.14.1/themes/south-street/jquery-ui.css";
+	$HC       = "https://code.highcharts.com/highcharts.js";
+	$HCmore   = 'https://code.highcharts.com/highcharts-more.js';
+	$HCdd     = 'https://code.highcharts.com/modules/drilldown.js';
+	$HCexp    = 'https://code.highcharts.com/modules/exporting.js';
+	$HCann    = 'https://code.highcharts.com/modules/annotations.js';
 }
 
 ?>
